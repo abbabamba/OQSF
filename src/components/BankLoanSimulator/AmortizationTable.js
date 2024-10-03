1689306664571b2819fe56549106fc4426012614
abbabamba <babacarbamba11@gmail.com>
2024-10-03 12:03:05 +0000

    last update

--- src/components/BankLoanSimulator/AmortizationTable.js
+++ src/components/BankLoanSimulator/AmortizationTable.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import styles from './AmortizationTable.module.css';
 
-// Fonction pour formater les nombres avec des séparateurs de milliers
-const formatNumber = (number) => {
-  return new Intl.NumberFormat('fr-FR', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }).format(number);
-};
-
 const AmortizationTable = ({ schedule }) => (
   <div className={styles.tableContainer}>
     <table className={styles.table}>
@@ -27,10 +19,10 @@ const AmortizationTable = ({ schedule }) => (
           <tr key={index}>
             <td>{row.period}</td>
             <td>{row.date}</td>
-            <td>{formatNumber(row.payment)} F CFA</td>
-            <td>{formatNumber(row.principal)} F CFA</td>
-            <td>{formatNumber(row.interest)} F CFA</td>
-            <td>{formatNumber(row.balance)} F CFA</td>
+            <td>{row.payment}</td>
+            <td>{row.principal}</td>
+            <td>{row.interest}</td>
+            <td>{row.balance}</td>
           </tr>
         ))}
       </tbody>
